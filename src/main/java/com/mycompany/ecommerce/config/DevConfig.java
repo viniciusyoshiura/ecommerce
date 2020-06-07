00924e6121c2f1d49429bc7a9470ff8b73a98846
viniciusyoshiura <viny.yoshiura@gmail.com>
2020-06-07 15:00:14 -0300

    Creating dev profile

---
+++ src/main/java/com/mycompany/ecommerce/config/DevConfig.java
@@ -0,0 +1,35 @@
+package com.mycompany.ecommerce.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mycompany.ecommerce.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	DBService dbService;
+
+	// ---------- Getting value from application-xxx.properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equalsIgnoreCase(strategy)) {
+			return false;
+		}
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}

f4e72bac62a80dcb6a1e14c263f8d008d89f1f62
viniciusyoshiura <viny.yoshiura@gmail.com>
2020-06-21 16:54:12 -0300

    Allowing CORS request for DELETE and PUT

--- src/main/java/com/mycompany/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/mycompany/ecommerce/config/SecurityConfig.java
@@ -104,8 +104,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// ---------- Allowing basic access for all endpoints
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
+		
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		// ---------- Methods which CORS is allowed
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
 
